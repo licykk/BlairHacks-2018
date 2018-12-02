e81bb86d789b2029fc4e8caabd438be7c52ea21f
Adam <ithinkiamadam@gmail.com>
2018-12-01 20:27:11 -0500

    Fix activity flow (Main to Team, not Game)

--- FaceTag/app/src/main/java/com/example/licykk/facetag/MainActivity.java
+++ FaceTag/app/src/main/java/com/example/licykk/facetag/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGame(View view) {
-        Intent intent = new Intent(this, GameActivity.class);
+        Intent intent = new Intent(this, TeamActivity.class);
         startActivity(intent);
     }
 }
